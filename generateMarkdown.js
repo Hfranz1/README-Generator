762406f4736f8ffe829993498a93b44e51c92ea8
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-10-30 21:56:23 -0600

    function for if there is no license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      break;
+    case "BSD 3":
+      return "![License: BSD 3](https://img.shields.io/badge/License-BSD%203-Clause-blue.svg)";
+      break;
+    case "None":
+      return "";
+      break;
+    default: "";
+      break;
+    case "APACHE 2.0":
+      return "![License: APACHE 2.0](https://img.shields.io/badge/License-APACHE%202.0-red.svg)";
+      break;
+    case "GPL 3.0":
+      return "![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-green.svg)";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {

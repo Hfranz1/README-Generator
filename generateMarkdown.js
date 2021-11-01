0d8fdc13f384e3c9ac2eb25cf3c73e758d0a2422
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-11-01 09:06:23 -0600

    license links

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -22,9 +22,30 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https:/opensource.org/licenses/MIT)";
+      break;
+    case "BSD 3":
+      return "]![License: BSD 3](https://img.shields.io/badge/License-BSD%203-Clause-blue.svg)](https:/opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "none":
+      return "";
+      break;
+    default:
+      "";
+      break;
+    case "APACHE 2.0":
+      return "[![License: APACHE 2.0](https://img.shields.io/badge/License-APACHE%202.0-red.svg)](https:/opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GOL 3.0":
+      return "[![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-green.svg)]((https:/opensource.org/licenses/gpl-3.0)";
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
